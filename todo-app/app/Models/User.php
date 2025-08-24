b075f28e903df07e9997d20ce6e64f6f8dab6198
Pawel Waszak <pawel@bits4u.pl>
2025-08-24 21:28:06 +0200

    Feat: added user tasks (one to many) relationship

--- todo-app/app/Models/User.php
+++ todo-app/app/Models/User.php
@@ -44,4 +44,7 @@ User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
